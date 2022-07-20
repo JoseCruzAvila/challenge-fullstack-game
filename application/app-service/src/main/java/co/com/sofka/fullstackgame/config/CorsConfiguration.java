2eea80eb7443ee996324054edea199299efedd3c
JoseCruzAvila <jose.cruz@sofka.com.co>
2022-07-20 18:25:33 -0500

    Change CorsConfig

--- application/app-service/src/main/java/co/com/sofka/fullstackgame/config/CorsConfiguration.java
+++ application/app-service/src/main/java/co/com/sofka/fullstackgame/config/CorsConfiguration.java
@@ -4,8 +4,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.config.CorsRegistry;
-import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @Configuration
 public class CorsConfiguration {
@@ -13,8 +14,8 @@ public class CorsConfiguration {
     private String origin;
 
     @Bean
-    public WebFluxConfigurer corsConfigurer() {
-        return new WebFluxConfigurer() {
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 if (!origin.isBlank()) {
